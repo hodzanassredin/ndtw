89449df9834bdd1c0cf2cc2d68c8b5d6b0c0af6e
Shelin Anton <shelinanton@gmail.com>
2021-01-15 16:31:06 +0300

    renaming

--- FastDtw.Tests/FastDtwTests.cs
+++ FastDtw.Tests/FastDtwTests.cs
@@ -4,7 +4,7 @@ using Newtonsoft.Json;
 using System.IO;
 using Xunit;
 
-namespace Fastdtw.Tests
+namespace NFastdtw.Tests
 {
     public class FastDtwTests
     {
--- NFastDtw/Fastdtw.cs
+++ NFastDtw/Fastdtw.cs
@@ -25,7 +25,7 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 
-namespace Fastdtw
+namespace NFastdtw
 {
     public class Fastdtw
     {
