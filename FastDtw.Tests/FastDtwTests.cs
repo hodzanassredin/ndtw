4acffbf6d9ad9945796c9d50d0b95dceaf08d23d
Shelin Anton <shelinanton@gmail.com>
2021-02-20 16:51:37 +0300

    cleanup

--- FastDtw.Tests/FastDtwTests.cs
+++ FastDtw.Tests/FastDtwTests.cs
@@ -13,9 +13,8 @@ namespace NFastdtw.Tests
         public void ShouldCorrectlyCalculateDtw()
         {
             var data = JsonConvert.DeserializeObject<TestData>(File.ReadAllText("test.json"));
-            //var result = new Fastdtw(data.x, data.y).GetCost();
-            var result = new Fastdtw(data.x, data.y, (x, y) => (new DenseVector(x) - new DenseVector(y)).L2Norm()).GetCost();
-            Assert.Equal(1.085367424306426, result);
+            var res2 = FastDtw.FastDtw.Distance(data.x, data.y, 5, (x, y) => (new DenseVector(x) - new DenseVector(y)).L2Norm());
+            Assert.Equal(1.0853674243064257, res2);
         }
         private class TestData
         {
@@ -23,6 +22,50 @@ namespace NFastdtw.Tests
             public double[][] y { get; set; }
         }
 
+        //public class Point
+        //{
+        //    public double Latitude { get; set; }
+        //    public double Longitude { get; set; }
+        //}
+
+        //static String BytesToString(long byteCount)
+        //{
+        //    string[] suf = { "B", "KB", "MB", "GB", "TB", "PB", "EB" }; //Longs run out around EB
+        //    if (byteCount == 0)
+        //        return "0" + suf[0];
+        //    long bytes = Math.Abs(byteCount);
+        //    int place = Convert.ToInt32(Math.Floor(Math.Log(bytes, 1024)));
+        //    double num = Math.Round(bytes / Math.Pow(1024, place), 1);
+        //    return (Math.Sign(byteCount) * num).ToString() + suf[place];
+        //}
+
+        //private static (T, string) CheckTimeAndMem<T>(string name, Func<T> f) {
+        //    GC.Collect();
+        //    var mem = GC.GetTotalMemory(false);
+        //    var watch = System.Diagnostics.Stopwatch.StartNew();
+        //    var result = f();
+        //    watch.Stop();
+        //    var elapsedS = watch.ElapsedMilliseconds/1000;
+        //    var memRep = $"{name}\tMemory={BytesToString(GC.GetTotalMemory(false) - mem)} \tTime s={elapsedS}";
+        //    return (result, memRep);
+        //}
+
+        //[Fact]
+        //public void ShouldCorrectlyCalculateDtwForBigArray()
+        //{
+        //    using (var reader = new StreamReader("test2.csv"))
+        //    using (var csv = new CsvReader(reader, CultureInfo.InvariantCulture))
+        //    {
+        //        var records = csv.GetRecords<Point>().ToArray();
+        //        var x = records.Select(x => new double[] { x.Latitude, x.Longitude }).ToArray();
+        //        var y = x.Select(a => a.Select(b => b).ToArray()).Take(2000).ToArray();
+
+        //        var r1 = CheckTimeAndMem("dtw", () => new Fastdtw(x, y, (x, y) => (new DenseVector(x) - new DenseVector(y)).L2Norm()).GetCost());
+        //        var r2 = CheckTimeAndMem("fastdtw", () => FastDtw.FastDtw.Distance(x, y, 5, (x, y) => (new DenseVector(x) - new DenseVector(y)).L2Norm()));
+
+        //    }
+           
+        //}
     }
 
 
--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-Fork of [NDtw](https://nuget.org/packages/NDtw) to match python's fastdtw api and support MathNet Numerics distance functions
+Fork of [fastdtw](https://bitbucket.org/0x6a62/fastdtw/src/dev/) to support multivariate series and custom distance functions
 
     using MathNet.Numerics.LinearAlgebra.Double;
     using Newtonsoft.Json;
@@ -14,11 +14,8 @@ Fork of [NDtw](https://nuget.org/packages/NDtw) to match python's fastdtw api an
             public void ShouldCorrectlyCalculateDtw()
             {
                 var data = JsonConvert.DeserializeObject<TestData>(File.ReadAllText("test.json"));
-                var result = new Fastdtw(
-                    data.x, 
-                    data.y, 
-                    (x, y) => (new DenseVector(x) - new DenseVector(y)).L2Norm()).GetCost();
-                Assert.Equal(1.085367424306426, result);
+                var res2 = FastDtw.FastDtw.Distance(data.x, data.y, 5, (x, y) => (new DenseVector(x) - new DenseVector(y)).L2Norm());
+                Assert.Equal(1.0853674243064257, result);
             }
             private class TestData
             {
@@ -30,18 +27,6 @@ Fork of [NDtw](https://nuget.org/packages/NDtw) to match python's fastdtw api an
         
     }
 
-
-It is the same as this NDtw code:
-
-    private static double[] SelectDim(double[][] arr, int dim) {
-            return arr.Select(x => x[dim]).ToArray();
-    }
-    var series = new[] {
-        new SeriesVariable(SelectDim(x, 0), SelectDim(y, 0)),
-        new SeriesVariable(SelectDim(x, 1), SelectDim(y, 1)),
-    };
-    return new Dtw(series, DistanceMeasure.Euclidean).GetCost();
-
 License
 ====
-NDtw is released under the MIT license: www.opensource.org/licenses/MIT
+Released under the MIT license: www.opensource.org/licenses/MIT
